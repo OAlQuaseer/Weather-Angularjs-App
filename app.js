d844af0a498b59abd2dac85cf3c1da5eec899c39
OAlQuaseer <omarqf87@gmail.com>
2016-04-26 17:01:53 -0700

    Adding my controllers and my routing configurations.

--- app.js
+++ app.js
@@ -1,2 +1,34 @@
 // My Module for my application.
 var myApp = angular.module("weatherApp", ["ngRoute","ngResource"]);
+
+
+//Configuration
+myApp.config(function($routeProvider){
+    
+    $routeProvider
+    
+    .when('/', {
+        templateUrl:'pagesList/home.html',
+        controller : 'homeController'
+    })
+    .when('/forecast', {
+       templateUrl:'pagesList/forecast.html',
+        controller : 'forecastController'
+        
+    })    
+});
+
+
+//Controllers
+myApp.controller('homeController',['$scope',function($scope){
+    
+    
+    
+    
+}]);
+myApp.controller('forecastController',['$scope',function($scope){
+    
+    
+    
+    
+}]);
